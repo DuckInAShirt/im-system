e352779b64acad749f21aab7719421dc884aaaea
= <=>
2025-11-09 22:59:24 +0800

    feat(*): 新增了用户登录、查看当前所有在线用户的功能

---
+++ server.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"sync"
+)
+
+type Server struct {
+	IP        string
+	Port      int
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+	Message   chan string
+}
+
+const BUFFER_LEN int = 4096
+
+// 启动服务器
+func (server *Server) start() {
+	fmt.Println("初始化聊天服务器...")
+	// 创建socket连接
+	listener, err := net.Listen("tcp4", fmt.Sprintf("%s:%d", server.IP, server.Port))
+	if err != nil {
+		fmt.Println("[server.start] [net.Listen] err: ", err)
+		return
+	}
+
+	go server.BroadCastChannelOnMessage()
+
+	// 关闭连接
+	defer listener.Close()
+
+	// 接收消息并调用回调函数处理消息
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("[server.start] [listener.Accept] err: ", err)
+			return
+		}
+		fmt.Printf("已建立和[%s]的连接\n", conn.RemoteAddr().String())
+		go server.ConnHandler(conn)
+	}
+}
+
+// 创建服务器
+func NewServer(IP string, Port int) *Server {
+	server := &Server{
+		IP:        IP,
+		Port:      Port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+	return server
+}
+
+// 处理连接的回调函数
+func (server *Server) ConnHandler(conn net.Conn) {
+	// 创建一个User连接
+	user := NewUser(conn, server)
+
+	// 用户上线
+	user.Online()
+
+	go server.ReceiveMessage(conn, user)
+
+	select {}
+}
+
+// 接收客户端发送的消息
+func (server *Server) ReceiveMessage(conn net.Conn, user *User) {
+	for {
+		buf := make([]byte, BUFFER_LEN)
+		n, err := conn.Read(buf)
+
+		// n == 0 连接关闭
+		if n == 0 {
+			user.Offline()
+			return
+		}
+
+		// err = io.EOF 也表示连接关闭
+		if err != nil && err != io.EOF {
+			fmt.Println("Conn Read error: ", err)
+			return
+		}
+
+		// 提取消息，去掉末尾的\n
+		msg := string(buf[:n-1])
+		if msg == "who" {
+			user.CheckOnlineUsers()
+		} else {
+			user.SendToBroadCastChannel(msg)
+		}
+	}
+}
+
+// 广播Channel监听消息
+func (server *Server) BroadCastChannelOnMessage() {
+	fmt.Println("广播channel已就绪...")
+	for {
+		message := <-server.Message
+		func() {
+			server.mapLock.RLock()
+			defer server.mapLock.RUnlock()
+			for _, cli := range server.OnlineMap {
+				cli.C <- message
+			}
+		}()
+	}
+}
---
+++ user.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type User struct {
+	Conn   net.Conn
+	C      chan string
+	Name   string
+	Addr   string
+	server *Server
+}
+
+// 创建User
+func NewUser(conn net.Conn, server *Server) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Conn:   conn,
+		C:      make(chan string),
+		Name:   userAddr,
+		Addr:   userAddr,
+		server: server,
+	}
+	go user.listenMessage()
+	return user
+}
+
+// 监听 User 的 channel，一旦有消息，就直接发送给客户端
+func (user *User) listenMessage() {
+	for {
+		message := <-user.C
+		user.Conn.Write([]byte(message + "\n"))
+	}
+}
+
+// 用户上线
+func (user *User) Online() {
+	user.server.mapLock.Lock()
+	defer user.server.mapLock.Unlock()
+	user.server.OnlineMap[user.Name] = user
+	user.SendToBroadCastChannel("已上线")
+}
+
+// 用户下线
+func (user *User) Offline() {
+	user.server.mapLock.Lock()
+	defer user.server.mapLock.Unlock()
+	delete(user.server.OnlineMap, user.Name)
+	user.SendToBroadCastChannel("已下线")
+}
+
+// 发送消息到广播Channel
+func (user *User) SendToBroadCastChannel(message string) {
+	fmt.Printf("Broadcast channel receives message from [%s]: %s\n", user.Name, message)
+	user.server.Message <- fmt.Sprintf("[%s]: %s", user.Name, message)
+}
+
+// 发送消息到user客户端
+func (user *User) SendMessageToClient(message string) {
+	user.C <- message
+}
+
+// 查看当前所有在线用户
+func (user *User) CheckOnlineUsers() {
+	user.server.mapLock.RLock()
+	defer user.server.mapLock.RUnlock()
+	for _, onlineUser := range user.server.OnlineMap {
+		message := fmt.Sprintf("[%s]%s: 在线...", onlineUser.Addr, onlineUser.Name)
+		user.SendMessageToClient(message)
+	}
+}
